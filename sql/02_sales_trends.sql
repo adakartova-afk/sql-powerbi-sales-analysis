0ef6b309ba906f957ea3b7c270ae32f8e5dd441a
adakartova-afk <ada.kartova@gmail.com>
2026-03-31 03:51:34 +0500

    Update 02_sales_trends.sql
    add sales trends SQL file

--- sql/02_sales_trends.sql
+++ sql/02_sales_trends.sql
@@ -1,1 +1,30 @@
+SELECT 
+    substr(o.order_purchase_timestamp, 1, 7) AS month,
+    SUM(oi.price) AS revenue
+FROM orders o
+JOIN order_items oi
+    ON o.order_id = oi.order_id
+GROUP BY month
+ORDER BY month;
 
+SELECT 
+    substr(order_purchase_timestamp, 1, 7) AS month,
+    COUNT(DISTINCT order_id) AS total_orders
+FROM orders
+GROUP BY month
+ORDER BY month;
+
+WITH monthly_revenue AS (
+    SELECT 
+        substr(o.order_purchase_timestamp, 1, 7) AS month,
+        SUM(oi.price) AS revenue
+    FROM orders o
+    JOIN order_items oi
+        ON o.order_id = oi.order_id
+    GROUP BY month
+)
+SELECT 
+    month,
+    ROUND(revenue, 2) AS revenue
+FROM monthly_revenue
+ORDER BY revenue DESC;
